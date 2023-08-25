2723bf06bde8886ac14daf5f71749c7ffebe6f03
SavageHawk7 <savagehawk7@outlook.com>
2023-08-25 11:07:49 -0500

    Initial Commit

---
+++ src/unl/soc/Hello.java
@@ -0,0 +1,17 @@
+package unl.soc;
+
+/**
+ * Author: James Hawkins (jhawkins18@huskers.unl.edu) 
+ * Date: 08/22/2023
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author JamesHawkins1
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("James Hawkins");
+	}
+}
